4ddf6fe1b15f3fac966e4b768ea13cf49935f614
Vishal_SR <52326540+Vishal-SR-505@users.noreply.github.com>
2024-03-12 10:12:33 +0530

    Delete react-form/react-form directory

--- react-form/react-form/src/Component/Label.js
+++
@@ -1,8 +0,0 @@
-import React, {useState,useCallback} from 'react';
-import classes from './Form.module.css'
-const Label = ({children}) =>{
-
-    return(<><label className={classes.label} for={children}>{children}</label></>)
-}
-
-export default Label
